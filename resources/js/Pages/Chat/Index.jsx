7cd8cea6a00cabb7c64b4213699bfdb1ebb7aaf3
raprmdn <rafiputraramadhan14@gmail.com>
2023-09-05 12:20:24 +0700

    [WIP] Chat UI

--- resources/js/Pages/Chat/Index.jsx
+++ resources/js/Pages/Chat/Index.jsx
@@ -55,7 +55,7 @@ export default function Index({auth}) {
                                     <div className="grid grid-cols-12">
 
                                         {/* Date Indicator */}
-                                        <div className="col-start-1 col-end-12 my-3">
+                                        <div className="col-start-1 col-end-13 my-3">
                                             <div className="flex items-center justify-center">
                                                 <div className="w-full h-0.5 bg-gray-800/70"></div>
                                                 <div className="px-4 py-0.5 text-[10.2px] text-gray-400 border border-gray-700 rounded bg-gray-800/40 tracking-wide leading-relaxed">
@@ -86,6 +86,19 @@ export default function Index({auth}) {
                                         <div className="col-start-1 col-end-9 p-1">
                                             <div className="flex flex-row">
                                                 <div className="py-2 pl-3 pr-12 text-sm text-gray-300 bg-gray-800 rounded-md">
+                                                    {/* Replied Chat */}
+                                                    <div className="max-w-full px-2 py-1.5 mb-1.5 bg-gray-700/50 border-gray-600 border-l-4 rounded">
+                                                        <div className="text-xs">
+                                                            <div className="mb-1 text-purple-400">
+                                                                Rafi Putra Ramadhan
+                                                            </div>
+                                                            <div className="overflow-hidden text-gray-300/80" style={{ display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical" }}>
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. 
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                     Lorem ipsum dolor sit Lorem ipsum dolor sit voluptatum.
                                                     <div className="flex items-center justify-end -mt-2.5 -mr-9">
                                                         <div className="text-[10px] text-gray-400/70">
@@ -112,19 +125,35 @@ export default function Index({auth}) {
                                         <div className="col-start-6 col-end-13 p-1">
                                             <div className="relative flex flex-row-reverse">
                                                 <div className="py-2 pl-3 pr-20 text-sm text-gray-300 bg-gray-800 rounded-md">
-                                                        Lorem.
-                                                        <div className="flex items-center justify-end -mt-2.5 -mr-16">
-                                                            <div className="text-[10px] text-gray-400/70">
-                                                                12:30
+                                                    {/* Replied Chat */}
+                                                    <div className="max-w-full px-2 py-1.5 mb-1.5 bg-gray-700/50 border-gray-600 border-l-4 rounded">
+                                                        <div className="text-xs">
+                                                            <div className="mb-1 text-purple-400">
+                                                                Rafi Putra Ramadhan
                                                             </div>
-                                                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" id="double-check" className="w-4 h-4 ml-1 -mr-2 text-cyan-500">
-                                                                <path fillRule="evenodd" d="M16.5303 6.46967C16.8232 6.76256 16.8232 7.23744 16.5303 7.53033L6.53033 17.5303C6.38968 17.671 6.19891 17.75 6 17.75 5.80109 17.75 5.61032 17.671 5.46967 17.5303L1.46967 13.5303C1.17678 13.2374 1.17678 12.7626 1.46967 12.4697 1.76256 12.1768 2.23744 12.1768 2.53033 12.4697L6 15.9393 15.4697 6.46967C15.7626 6.17678 16.2374 6.17678 16.5303 6.46967zM22.5303 6.46966C22.8232 6.76254 22.8232 7.23742 22.5303 7.53032L12.5308 17.5303C12.2379 17.8232 11.7631 17.8232 11.4702 17.5304L9.96975 16.0304C9.67681 15.7376 9.67674 15.2627 9.96959 14.9697 10.2624 14.6768 10.7373 14.6767 11.0303 14.9696L12.0004 15.9394 21.4697 6.46968C21.7625 6.17678 22.2374 6.17677 22.5303 6.46966z" clipRule="evenodd"></path>
-                                                            </svg>
+                                                            <div className="overflow-hidden text-gray-300/80" style={{ display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical" }}>
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. 
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                            </div>
                                                         </div>
                                                     </div>
+                                                    {/* Chat Body */}
+                                                    Lorem.
+                                                    {/* Sent Timestamp */}
+                                                    <div className="flex items-center justify-end -mt-2.5 -mr-16">
+                                                        <div className="text-[10px] text-gray-400/70">
+                                                            12:30
+                                                        </div>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24" id="double-check" className="w-4 h-4 ml-1 -mr-2 text-cyan-500">
+                                                            <path fillRule="evenodd" d="M16.5303 6.46967C16.8232 6.76256 16.8232 7.23744 16.5303 7.53033L6.53033 17.5303C6.38968 17.671 6.19891 17.75 6 17.75 5.80109 17.75 5.61032 17.671 5.46967 17.5303L1.46967 13.5303C1.17678 13.2374 1.17678 12.7626 1.46967 12.4697 1.76256 12.1768 2.23744 12.1768 2.53033 12.4697L6 15.9393 15.4697 6.46967C15.7626 6.17678 16.2374 6.17678 16.5303 6.46967zM22.5303 6.46966C22.8232 6.76254 22.8232 7.23742 22.5303 7.53032L12.5308 17.5303C12.2379 17.8232 11.7631 17.8232 11.4702 17.5304L9.96975 16.0304C9.67681 15.7376 9.67674 15.2627 9.96959 14.9697 10.2624 14.6768 10.7373 14.6767 11.0303 14.9696L12.0004 15.9394 21.4697 6.46968C21.7625 6.17678 22.2374 6.17677 22.5303 6.46966z" clipRule="evenodd"></path>
+                                                        </svg>
+                                                    </div>
+                                                </div>
                                                 <div className="absolute top-0 w-4 h-4 bg-gray-800 rounded-br-full -right-2"></div>
                                             </div>
                                         </div>
+                                        
                                         <div className="col-start-6 col-end-13 p-1">
                                             <div className="flex flex-row-reverse">
                                                 <div className="py-2 pl-3 pr-20 text-sm text-gray-300 bg-gray-800 rounded-md">
@@ -157,7 +186,7 @@ export default function Index({auth}) {
                                         </div>
 
                                         {/* Date Indicator */}
-                                        <div className="col-start-1 col-end-12 my-3">
+                                        <div className="col-start-1 col-end-13 my-3">
                                             <div className="flex items-center justify-center">
                                                 <div className="w-full h-0.5 bg-gray-800/70"></div>
                                                 <div className="px-4 py-0.5 text-[10.2px] text-gray-400 border border-gray-700 rounded bg-gray-800/40 tracking-wide leading-relaxed">
@@ -172,6 +201,19 @@ export default function Index({auth}) {
                                             <div className="flex flex-row">
                                                 <div className="relative flex">
                                                     <div className="py-2 pl-3 pr-12 text-sm text-gray-300 bg-gray-800 rounded-md">
+                                                        {/* Replied Chat */}
+                                                        <div className="max-w-full px-2 py-1.5 mb-1.5 bg-gray-700/50 border-gray-600 border-l-4 rounded">
+                                                            <div className="text-xs">
+                                                                <div className="mb-1 text-purple-400">
+                                                                    Rafi Putra Ramadhan
+                                                                </div>
+                                                                <div className="overflow-hidden text-gray-300/80" style={{ display: "-webkit-box", WebkitLineClamp: 2, WebkitBoxOrient: "vertical" }}>
+                                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum. 
+                                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
                                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
                                                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
